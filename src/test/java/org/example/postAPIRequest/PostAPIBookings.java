eca5106dd26808c9c7b561330a8991faa2bab660
Program900 <gitahemanth@gmail.com>
2024-03-20 22:51:16 +0000

    added post request

--- src/test/java/org/example/postAPIRequest/PostAPIBookings.java
+++ src/test/java/org/example/postAPIRequest/PostAPIBookings.java
@@ -3,6 +3,7 @@ package org.example.postAPIRequest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import net.minidev.json.JSONObject;
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
 public class PostAPIBookings {
@@ -24,9 +25,12 @@ public class PostAPIBookings {
         requestBody.put("additionalneeds", "Breakfast");
         RestAssured.given().contentType(ContentType.JSON)
                 .baseUri("https://restful-booker.herokuapp.com").body(requestBody.toJSONString()).
-                log().body().
+                log().all().
                 when().post("/booking").
-                then().assertThat().statusCode(200).body("booking.firstname", org.hamcrest.Matchers.equalTo("Jim")).
+                then().assertThat().
+                log().body().
+                statusCode(200).
+                body("booking.firstname", org.hamcrest.Matchers.equalTo("Jim")).
                 body("booking.lastname", org.hamcrest.Matchers.equalTo("Brown")).
                 body("booking.totalprice", org.hamcrest.Matchers.equalTo(111)).
                 body("booking.depositpaid", org.hamcrest.Matchers.equalTo(true)).
@@ -34,5 +38,29 @@ public class PostAPIBookings {
                 body("booking.bookingdates.checkout", org.hamcrest.Matchers.equalTo("2021-07-01")).
                 body("booking.additionalneeds", org.hamcrest.Matchers.equalTo("Breakfast"));
 
+    }
+    @Test
+        //prepare the request body  for the booking api https://restful-booker.herokuapp.com booking api and create a booking using post request method and validate the response
+
+    void createBookingForValidationFails(){
+        System.out.println("Create booking");
+        JSONObject requestBody = new JSONObject();
+        JSONObject bookingDates = new JSONObject();
+
+        requestBody.put("firstname", "Jim");
+        requestBody.put("lastname", "Brown");
+        requestBody.put("totalprice", 111);
+        requestBody.put("depositpaid", true);
+        requestBody.put("bookingdates", bookingDates);
+        bookingDates.put("checkin", "2021-07-01");
+        bookingDates.put("checkout", "2021-07-01");
+        requestBody.put("additionalneeds", "Breakfast");
+        RestAssured.given().contentType(ContentType.JSON)
+                .baseUri("https://restful-booker.herokuapp.com").body(requestBody.toJSONString()).
+                when().post("/booking").
+                then().assertThat().
+                log().ifValidationFails().
+                statusCode(200).
+                body("booking.firstname", Matchers.equalTo("Jim1"));
     }
 }
