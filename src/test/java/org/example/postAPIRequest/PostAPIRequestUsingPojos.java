e2ca4e69f4c07b5d67c3b603c0fade88c2f53466
Program900 <gitahemanth@gmail.com>
2024-03-22 15:45:17 +0000

    added Deserialization

--- src/test/java/org/example/postAPIRequest/PostAPIRequestUsingPojos.java
+++ src/test/java/org/example/postAPIRequest/PostAPIRequestUsingPojos.java
@@ -1,6 +1,9 @@
 package org.example.postAPIRequest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.example.pojos.Booking;
 import org.example.pojos.BookingDates;
 import org.testng.annotations.Test;
@@ -27,20 +30,40 @@ public class PostAPIRequestUsingPojos {
             System.out.println(requestBody);
             
             //Deserialization - JSON to Java Object - convert the JSON string to a booking object
-            Booking booking1 = objectMapper.readValue(requestBody, Booking.class);
-            //print the booking firstname
-            System.out.println(booking1.getFirstname());
-            //print the booking totalprice
-            System.out.println(booking1.getTotalprice());
-            //print the booking checkin date
-            System.out.println(booking1.getBookingdates().getCheckin());
-            //print the booking checkout date
-            System.out.println(booking1.getBookingdates().getCheckout());
+            Booking bookingDetails = objectMapper.readValue(requestBody, Booking.class);
+            getBookingDetails(bookingDetails);
+            //get response from the post request
+                Response response = RestAssured.given().contentType(ContentType.JSON).baseUri("https://restful-booker.herokuapp.com").body(requestBody).log().all().
+                    when().post("/booking").
+                    then().assertThat().log().body().statusCode(200).extract().response();
+            //get the booking id from the response body using the path method
+            int bookingId = response.path("bookingid");
+            //print the booking id
+            System.out.println("Booking id is: " + bookingId);
+            //get the booking details using the booking id
+            Response getResponse = RestAssured.given().contentType(ContentType.JSON).baseUri("https://restful-booker.herokuapp.com").log().all().
+                    when().get("/booking/" + bookingId).
+                    then().assertThat().log().body().statusCode(200).extract().response();
+
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+
+
+    }
+
+
+    private void getBookingDetails(Booking bookingDetails) {
+        //print the booking firstname
+        System.out.println(bookingDetails.getFirstname());
+        //print the booking totalprice
+        System.out.println(bookingDetails.getTotalprice());
+        //print the booking checkin date
+        System.out.println(bookingDetails.getBookingdates().getCheckin());
+        //print the booking checkout date
+        System.out.println(bookingDetails.getBookingdates().getCheckout());
 
 
     }
